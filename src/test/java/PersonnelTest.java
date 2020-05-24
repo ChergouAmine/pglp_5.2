b92f1545f2872fa64c963279e18580ed67135a64
Utilisateur <user@debian.arcahe.ovh>
2020-05-24 15:22:44 +0200

    TEst PErsonnel

---
+++ src/test/java/PersonnelTest.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import org.junit.Test;
+
+public class PersonnelTest {
+  @Test
+  public void test() {
+      LocalDate date=LocalDate.now();
+      
+      Personnel p1=new Personnel.Builder("CHERGOU","Amine",date).build();
+      Personnel p2=new Personnel.Builder("ATTOUCHE","Maher",date).build();
+      Personnel p3=new Personnel.Builder("ARAR","Maher",date).build();
+      Personnel p4=new Personnel.Builder("BENAMAR","Ismail",date).build();
+      Personnel p5=new Personnel.Builder("BENMAALEM","Amir",date).build();
+      
+      CompositePersonnel l1=new CompositePersonnel(1);
+      CompositePersonnel l2=new CompositePersonnel(3);
+      CompositePersonnel l3=new CompositePersonnel(4);
+      
+      
+      l3.add(p1);
+      l3.add(p2);
+      
+      
+      l2.add(p3);
+      l2.add(l3);
+      
+      l1.add(p4);
+      l1.add(l2);
+      
+      
+      Groupe affGrp = new Groupe(l1);
+      for(Iterator iter = affGrp.getIterator();iter.hasNext();) {
+          iter.next().print();
+      }
+      
+  }
+}
