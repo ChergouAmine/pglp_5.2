d5c486f9cde17c1dae76acc96193a07eccad64a4
Utilisateur <user@debian.arcahe.ovh>
2020-05-24 15:12:52 +0200

    Ajout DAO 2

---
+++ src/main/java/DAOFactory.java
@@ -0,0 +1,10 @@
+
+public final class DAOFactory implements java.io.Serializable {
+    private static final long serialVersionUID = 25;
+    private DAOFactory() {
+    }
+    
+ public static DAO<Personnel> getPersonnelDAO() {
+        return new PersonnelDAO();
+    }
+}
